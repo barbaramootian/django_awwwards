0c9b1ce841b826c68022e02b5222d16c7167768d
barbaramootian <barbara.mootian@student.moringaschool.com>
2022-03-13 23:36:26 +0300

    model class for profile

--- awwardapp/models.py
+++ awwardapp/models.py
@@ -1,18 +1,18 @@
-# from django.db import models
-# from django.contrib.auth.models import User
+from django.db import models
+from django.contrib.auth.models import User
 
-# class Profile(models.Model):
-#     user=models.OneToOneField(User, on_delete=models.CASCADE)
-#     profile=models.ImageField(default="profile_pics/default.jpeg", upload_to='profile_pics')
-#     bio=models.CharField(max_length=200, blank=False)
-#     contact_information = models.TextField()
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    profile=models.ImageField(default="profile_pics/default.jpeg", upload_to='profile_pics')
+    bio=models.CharField(max_length=200, blank=False)
+    contact_information = models.TextField()
 
-#     def save_profile(self):
-#         self.save()
-#     def delete_profile(self):
-#         Image.objects.filter(id = self.pk).delete() 
-#     def update_profile(self, **kwargs):
-#         self.objects.filter(id = self.pk).update(**kwargs)
+    def save_profile(self):
+        self.save()
+    def delete_profile(self):
+        Image.objects.filter(id = self.pk).delete() 
+    def update_profile(self, **kwargs):
+        self.objects.filter(id = self.pk).update(**kwargs)
 
 
 
