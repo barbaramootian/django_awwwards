75c783920a0ef8671989a2b2d426db4fce3e7927
barbaramootian <barbara.mootian@student.moringaschool.com>
2022-03-13 23:30:46 +0300

    initial commit

---
+++ awwardapp/urls.py
@@ -0,0 +1,10 @@
+# from django.urls import path  
+# from django.contrib import admin  
+# from . import views  
+  
+# urlpatterns = [  
+#    path('',views.index, name='index'),
+#    path('accounts/profile/', views.index, name="index"),
+#    path('signout/', views.logoutUser, name='signout'),
+# ]
+  
---
+++ awwardapp/views.py
@@ -0,0 +1,13 @@
+# from django.shortcuts import render,redirect
+# from django.contrib.auth.decorators import login_required
+# from django.http import HttpResponse,Http404
+# from django.contrib .auth import logout
+# # Create your views here.
+# @login_required(login_url='/accounts/login/')
+# def index(request):
+#     return render(request,'index.html')
+
+
+# def logoutUser(request):
+#     logout(request)
+#     return redirect('index')   
